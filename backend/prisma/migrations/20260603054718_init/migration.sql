8604fbb31fd77a256a5284d9934ae9e98aba0f0f
___sai_nihal_____ <sainihal04@gmail.com>
2026-08-03 17:25:26 +0530

    Add primary key to session speaker migration

--- backend/prisma/migrations/20260603054718_init/migration.sql
+++ backend/prisma/migrations/20260603054718_init/migration.sql
@@ -111,7 +111,7 @@ CREATE TABLE `_SessionToSpeaker` (
     `A` VARCHAR(191) NOT NULL,
     `B` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `_SessionToSpeaker_AB_unique`(`A`, `B`),
+    PRIMARY KEY (`A`, `B`),
     INDEX `_SessionToSpeaker_B_index`(`B`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
